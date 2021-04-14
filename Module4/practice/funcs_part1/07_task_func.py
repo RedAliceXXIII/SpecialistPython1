1bffc938ebdf62fb328260cc7370ab902acd0cca
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-14 14:45:29 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,11 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+def my_time(sec):
+    hours = sec // 3600
+    minutes = (sec - hours * 3600) // 60
+    sec = sec - hours * 3600 - minutes * 60
+    return f'{hours:>2}:{minutes:>2}:{sec:>2}'
+
+
+print(my_time(10000))
